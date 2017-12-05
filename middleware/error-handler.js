0bf6e5e4a734f4953a9eff43b8943f5aae9a71e8
Brian Holland <neumaics@gmail.com>
2017-12-05 01:38:38 -0700

    added tests and docs

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,8 +1,22 @@
+const { JobNotFound, InvalidProgress } = require('../models/errors');
+
+const BAD_REQUEST = 400;
+const NOT_FOUND = 404;
+const INTERNAL = 500;
+
 class ErrorHandler {
   constructor() {
     return (err, req, res, next) => { // eslint-disable-line no-unused-vars
-      console.error(err);
-      res.status(500).json({ error: err.message });
+
+      if (err instanceof JobNotFound) {
+        res.status(NOT_FOUND);
+      } else if (err instanceof InvalidProgress) {
+        res.status(BAD_REQUEST);
+      } else {
+        res.status(INTERNAL);
+      }
+
+      res.json({ error: err.message });
     };
   }
 }
